175b596ae6679867ff1d7c4d259f17eb00c271b3
rmacarthur6618 <100454478+rmacarthur6618@users.noreply.github.com>
2023-04-29 11:54:28 -0700

    Add files via upload

---
+++ js/free-shipping.js
@@ -0,0 +1,14 @@
+var msg = '<div class="header">This item is eligible for free shipping!</div>';
+msg += '<input type="button" name="button" value="OK" class="buyButton">';
+
+var elFreeship = document.createElement('div');
+elFreeship.setAttribute('id', 'note');
+elFreeship.innerHTML = msg;
+document.body.appendChild(elFreeship);
+
+function dismissNote() {
+	document.body.removeChild(elFreeship);
+}
+
+var elClose = document.getElementById('buyButton');
+elClose.addEventListener('click', dismissNote, false);
